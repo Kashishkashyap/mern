e2d93e54d96b91153837a2a85503cb134c534f59
Kashish <kashyapkashish22@gmail.com>
2024-01-30 12:29:08 +0530

    card

--- frontend/src/components/coursedisplay.js
+++ frontend/src/components/coursedisplay.js
@@ -25,18 +25,37 @@ const CourseList = ({ onAddToBucket }) => {
     }, []);
 
     return (
+        // <div>
+        //     <h2>Available Courses</h2>
+        //     <ul class="container">
+        //         {courses && courses.map((course) => (
+
+
+        //             <li key={course._id}>
+        //                 <p>{course.title}</p>
+        //                 <button class="btn btn-info" onClick={() => onAddToBucket(course._id)}>Add to Bucket</button>
+        //             </li>
+        //         ))}
+        //     </ul>
+        // </div>
         <div>
             <h2>Available Courses</h2>
-            <ul class="container">
-                {courses && courses.map((course) => (
-
-
-                    <li key={course._id}>
-                        <p>{course.title}</p>
-                        <button class="btn btn-info" onClick={() => onAddToBucket(course._id)}>Add to Bucket</button>
-                    </li>
-                ))}
-            </ul>
+            <div className="container">
+                <div className="row">
+                    {courses && courses.map((course) => (
+                        <div key={course._id} className="col-md-4 mb-4">
+                            <div className="card">
+                                {/* <img src={course.image} className="card-img-top" alt={course.title} /> */}
+                                <div className="card-body">
+                                    <h5 className="card-title">{course.title}</h5>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis dicta velit in eligendi praesentium distinctio veritatis corporis! Cum, rem sequi.</p>
+                                    <button className="btn btn-info" onClick={() => onAddToBucket(course._id)}>Add to Bucket</button>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
